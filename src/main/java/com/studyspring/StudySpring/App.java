6f7fa2f12647a246fbbc07a81b5ed22526e18532
Almas Sinev <26almassinev@gmail.com>
2020-06-02 01:30:00 +0300

    Залили Spring

---
+++ src/main/java/com/studyspring/StudySpring/App.java
@@ -0,0 +1,13 @@
+package com.studyspring.StudySpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
